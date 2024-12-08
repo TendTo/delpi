d3e814781745ca0207e4588fb145a095607e3e5d
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-12-08 20:22:15 +0000

    feat: add intrusive_ptr

--- delpi/util/concepts.h
+++ delpi/util/concepts.h
@@ -14,7 +14,24 @@
 namespace delpi {
 
 /**
- * Check if the type T is an iterable type
+ * Check if the type `T` is a self-reference counter type, i.e. it has both `AddRef` and `Release` methods.
+ *
+ * The `AddRef` method should increment the internal reference counter.
+ * The `Release` method should decrement it, and if it reaches zero, the object should be deleted using its destructor.
+ * @code
+ * template <Iterable T>
+ * void foo(T t); // T can be any iterable type
+ * @endcode
+ * @tparam T type to check
+ */
+template <class T>
+concept SelfReferenceCounter = requires(T t) {
+  { t.AddRef() };
+  { t.Release() };
+};  // NOLINT(readability/braces) per C++ standard concept definition
+
+/**
+ * Check if the type `T` is an iterable type.
  * @code
  * template <Iterable T>
  * void foo(T t); // T can be any iterable type
@@ -28,7 +45,7 @@ concept Iterable = requires(T t) {
 };  // NOLINT(readability/braces) per C++ standard concept definition
 
 /**
- * Check if the type T is an iterable type with a size() method
+ * Check if the type `T` is an iterable type with a `size()` method.
  * @code
  * template <SizedIterable T>
  * void foo(T t); // T can be any iterable type with a size() method
@@ -43,7 +60,7 @@ concept SizedIterable = requires(T t) {
 };  // NOLINT(readability/braces) per C++ standard concept definition
 
 /**
- * Check if the type T is an iterable type with elements of type U
+ * Check if the type `T` is an iterable type with elements of type `U`.
  * @code
  * template <TypedIterable<int> T>
  * void foo(T t); // T can be any iterable type with elements of type int
@@ -58,7 +75,7 @@ concept TypedIterable = requires(T t, U u) {
 } && std::same_as<typename T::value_type, U>;
 
 /**
- * Check if the type T is an iterable type with elements of type U and a size() method
+ * Check if the type `T` is an iterable type with elements of type `U` and a `size()` method.
  * @code
  * template <TypedSizedIterable<int> T>
  * void foo(T t); // T can be any iterable type with elements of type int and a size() method
@@ -74,7 +91,7 @@ concept SizedTypedIterable = requires(T t, U u) {
 } && std::same_as<typename T::value_type, U>;
 
 /**
- * Check if the type T is any of the types U
+ * Check if the type `T` is any of the types `U`.
  * @code
  * template <IsAnyOf<int, float, bool> T>
  * void foo(T t); // T can be either int, float or bool
@@ -86,7 +103,7 @@ template <typename T, typename... U>
 concept IsAnyOf = (std::same_as<T, U> || ...);
 
 /**
- * Check if the type T is not any of the types U
+ * Check if the type `T` is not any of the types `U`.
  * @code
  * template <IsNotAnyOf<int, float, bool> T>
  * void foo(T t); // T can be any type except int, float or bool
@@ -98,7 +115,7 @@ template <typename T, typename... U>
 concept IsNotAnyOf = !IsAnyOf<T, U...>;
 
 /**
- * Check if the type T supports the arithmetic operations +, -, *, /
+ * Check if the type `T` supports the arithmetic operations `+`, `-`, `*`, `/`.
  * @code
  * template <Arithmetic T>
  * void foo(T a, T b); // a and b can be added, subtracted, multiplied and divided with the corresponding operator
@@ -114,7 +131,8 @@ concept Arithmetic = requires(T a, T b) {
 };  // NOLINT(readability/braces) per C++ standard concept definition
 
 /**
- * Check if the type T supports the arithmetic operations +, -, *, / and the comparison operators <, >, <=, >=
+ * Check if the type `T` supports the arithmetic operations `+`, `-`, `*`, `/`
+ * and the comparison operators `<, `>`, `<=`, `>=`.
  * @code
  * template <Numeric T>
  * void foo(T a); // a can be added, subtracted, multiplied, divided and ordered with the corresponding operator
---
+++ delpi/util/intrusive_ptr.hpp
@@ -0,0 +1,176 @@
+/**
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024 delpi
+ * @copyright Electronic Arts Inc. All rights reserved.
+ * @licence BSD 3-Clause License
+ * Utilities for filesystem operations.
+ *
+ * Simple utilities that make operations on the filesystem easier.
+ */
+#pragma once
+
+#include "delpi/util/concepts.h"
+
+namespace delpi {
+
+/**
+ * Pointer to a generic object that supports intrusive reference counting.
+ *
+ * Implementation based on the
+ * [EASTL intrusive_ptr](https://github.com/electronicarts/EASTL/blob/master/include/EASTL/intrusive_ptr.h).
+ * @tparam T class the intrusive_ptr is pointing to.
+ * Must have `AddRef` and `Release` methods.
+ */
+template <SelfReferenceCounter T>
+class intrusive_ptr {
+ public:
+  typedef T element_type;  ///< The type this intrusive_ptr points to.
+
+  /** @constructor{intrusive_ptr} */
+  intrusive_ptr() : ptr_{nullptr} {}
+
+  /**
+   * Construct a new intrusive_ptr object wrapping the given pointer @p p.
+   *
+   * If @p add_ref is true, the reference count of the object is incremented immediately.
+   * @code
+   * intrusive_ptr<MyObject> str(new MyObject());
+   * @endcode
+   * @param p pointer to the object
+   * @param add_ref whether to increment the reference count
+   */
+  explicit intrusive_ptr(T* p, const bool add_ref = true) : ptr_{p} {
+    if (ptr_ && add_ref) ptr_->AddRef();
+  }
+  /**
+   * Construct a new intrusive_ptr object from another intrusive_ptr object.
+   *
+   * `AddRef` is immediately run on the object.
+   * The source intrusive_ptr object maintains its `AddRef` on the pointer.
+   * @code
+   * intrusive_ptr<MyObject> my_object_ptr_1;
+   * intrusive_ptr<MyObject> my_object_ptr_2(my_object_ptr_1);
+   * @endcode
+   * @param ip intrusive_ptr object to copy
+   */
+  intrusive_ptr(const intrusive_ptr& ip) : ptr_{ip.ptr_} {
+    if (ptr_) ptr_->AddRef();
+  }
+  /**
+   * Construct a new intrusive_ptr object from another intrusive_ptr object using move semantics.
+   * @param ip intrusive_ptr object to move
+   */
+  intrusive_ptr(intrusive_ptr&& ip) noexcept : ptr_{ip.ptr_} { ip.ptr_ = nullptr; }
+
+  /** Destruct the intrusive_ptr object, releasing the owned pointer. */
+  ~intrusive_ptr() {
+    if (ptr_) ptr_->Release();
+  }
+
+  /**
+   * Copy assignment operator.
+   *
+   * `AddRef` is immediately run on the object.
+   * The source @p ip maintains its reference to the pointer.
+   * If this pointer was already in use, the old object is released at the end.
+   * @param ip intrusive_ptr object to copy
+   * @return reference to this intrusive_ptr object
+   */
+  intrusive_ptr& operator=(const intrusive_ptr& ip) {
+    intrusive_ptr{ip}.swap(*this);
+    return *this;
+  }
+
+  /**
+   * Move assignment operator.
+   * @param ip intrusive_ptr object to move
+   * @return reference to this intrusive_ptr object
+   */
+  intrusive_ptr& operator=(intrusive_ptr&& ip) noexcept {
+    intrusive_ptr(static_cast<intrusive_ptr&&>(ip)).swap(*this);
+    return *this;
+  }
+
+  /**
+   * Copy assignment operator.
+   *
+   * `AddRef` is immediately run on the object.
+   * If this pointer was already in use, the old object is released at the end.
+   * @param ptr raw pointer to the object
+   * @return reference to this intrusive_ptr object
+   */
+  intrusive_ptr& operator=(T* ptr) {
+    intrusive_ptr{ptr}.swap(*this);
+    return *this;
+  }
+
+  /** @getter{reference to the contained object, intrusive_ptr} */
+  T& operator*() const { return *ptr_; }
+  /**
+   * @getter{underlying raw pointer, intrusive_ptr,
+   * Allows this object to be used as if it were the underlying pointer.}
+   */
+  T* operator->() const { return ptr_; }
+  /** @getter{underlying raw pointer, intrusive_ptr} */
+  [[nodiscard]] T* get() const { return ptr_; }
+
+  /** Release the owned pointer, decrementing the reference count and setting @ref ptr_ to null. */
+  void reset() { intrusive_ptr{}.swap(*this); }
+
+  /**
+   * Exchanges the owned pointer between two intrusive_ptr objects.
+   * @param ip intrusive_ptr object to swap with
+   */
+  void swap(intrusive_ptr& ip) noexcept {
+    T* const pTemp = ptr_;
+    ptr_ = ip.ptr_;
+    ip.ptr_ = pTemp;
+  }
+
+  /**
+   * Sets the owned pointer to the given pointer @p pObject without incrementing the reference count.
+   *
+   * The intrusive_ptr eventually only does a `Release()` on the object.
+   * Useful for assuming a reference that someone else has handed you and making sure it is always released.
+   * @param ptr pointer to the object
+   */
+  void attach(T* ptr) {
+    T* const temp_ptr = ptr_;
+    ptr_ = ptr;
+    if (temp_ptr) temp_ptr->Release();
+  }
+
+  /**
+   * Surrenders the reference held by an intrusive_ptr pointer.
+   *
+   * It returns the current reference and nulls the pointer without decrementing the reference count.
+   * Therefore, if the returned pointer is non-null it must be eventually released by the caller.
+   * @return the current reference
+   */
+  T* detach() {
+    T* const pTemp = ptr_;
+    ptr_ = nullptr;
+    return pTemp;
+  }
+
+  typedef element_type* intrusive_ptr::* bool_;
+  /**
+   * Implicit bool conversion.
+   *
+   * Allows the intrusive_ptr to be used as a boolean in checks.
+   * @code
+   * intrusive_ptr<MyObject> ptr = new MyObject();
+   * if (ptr) ptr->DoSomething();
+   * @endcode
+   * @note The implementation does not use the builtin `bool()` operator.
+   * That is because boolean are implicitally converted to short, int, float, etc.
+   * Hence, you can have ambiguous code such as `if (ptr == 1) // True`.
+   */
+  operator bool_() const noexcept { return ptr_ ? &intrusive_ptr::ptr_ : nullptr; }
+  bool operator!() const { return ptr_ == nullptr; }
+
+ protected:
+  T* ptr_;
+};  // class intrusive_ptr
+
+}  // namespace delpi
