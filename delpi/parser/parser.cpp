368adca9e3f879dbcf136257fdf1fa9a366c93dd
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-12-16 10:48:44 +0000

    ref: remove parser class in favor of simple function

--- delpi/parser/parser.cpp
+++ delpi/parser/parser.cpp
@@ -3,9 +3,7 @@
  * @copyright 2024 delpi
  * @licence BSD 3-Clause License
  */
-#include "Parser.h"
-
-#include <fstream>
+#include "parser.h"
 
 #include "delpi/parser/mps/Driver.h"
 #include "delpi/util/error.h"
@@ -13,15 +11,13 @@
 namespace delpi {
 
 bool LpSolver::Parse() { return config_.read_from_stdin() ? ParseStream(std::cin) : ParseFile(config_.filename()); }
-bool LpSolver::ParseFile(const std::string& filename) { return Parser::GetDriverInstance(*this)->ParseFile(filename); }
+bool LpSolver::ParseFile(const std::string& filename) { return GetDriverInstance(*this)->ParseFile(filename); }
 bool LpSolver::ParseStream(std::istream& stream, const std::string& stream_name) {
-  return Parser::GetDriverInstance(*this)->ParseStream(stream, stream_name);
+  return GetDriverInstance(*this)->ParseStream(stream, stream_name);
 }
-bool LpSolver::ParseString(const std::string& string) { return Parser::GetDriverInstance(*this)->ParseString(string); }
+bool LpSolver::ParseString(const std::string& string) { return GetDriverInstance(*this)->ParseString(string); }
 
-Parser::Parser(LpSolver& lp_solver) : driver_(GetDriverInstance(lp_solver)) {}
-
-std::unique_ptr<Driver> Parser::GetDriverInstance(LpSolver& lp_solver) {
+std::unique_ptr<Driver> GetDriverInstance(LpSolver& lp_solver) {
   switch (lp_solver.config().actual_format()) {
     case Config::Format::MPS:
       return std::make_unique<mps::MpsDriver>(lp_solver);
@@ -29,7 +25,5 @@ std::unique_ptr<Driver> Parser::GetDriverInstance(LpSolver& lp_solver) {
       DELPI_UNREACHABLE();
   }
 }
-
-const Stats& Parser::stats() const { return driver_->stats(); }
 
 }  // namespace delpi
--- delpi/parser/parser.h
+++ delpi/parser/parser.h
@@ -10,37 +10,14 @@
 
 #include "delpi/parser/Driver.h"
 #include "delpi/solver/LpSolver.h"
-#include "delpi/util/Stats.h"
 
 namespace delpi {
 
 /**
- * Facade class that hides the implementation details of the parser.
- * Parsers usually utilise a driver, a parser and a scanner to go through the input file and populate the LpSolver.
- * The correct driver is selected based on the actual format set in the Config.
+ * Get the correct driver instance based on the actual format set in the Config.
+ * @param lp_solver LP solver to populate
+ * @return A new instance of the correct driver based on the actual format set in the Config
  */
-class Parser {
- public:
-  /**
-   * Construct a new Parser object to populate the given `lp_solver`.
-   * @param lp_solver LP solver to populate
-   */
-  explicit Parser(LpSolver& lp_solver);
-
-  /**
-   * Get the correct driver instance based on the actual format set in the Config.
-   * @param lp_solver LP solver to populate
-   * @return A new instance of the correct driver based on the actual format set in the Config
-   */
-  static std::unique_ptr<Driver> GetDriverInstance(LpSolver& lp_solver);
-
-  /** @getter{stats, driver} */
-  [[nodiscard]] const Stats& stats() const;
-  /** @getter{driver, driver} */
-  [[nodiscard]] const std::unique_ptr<Driver>& driver() const { return driver_; }
-
- private:
-  std::unique_ptr<Driver> driver_;  ///< Driver to use based on the input file format
-};
+std::unique_ptr<Driver> GetDriverInstance(LpSolver& lp_solver);
 
 }  // namespace delpi
