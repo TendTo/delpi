da190e75c7523b73829f62229b6c5219a0756565
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-01-28 14:10:38 +0000

    test: add tests for RetriangularisationFactor

---
+++ delpi/solver/internal/RetriangularisationFactor.cpp
@@ -0,0 +1,89 @@
+#include "delpi/solver/internal/RetriangularisationFactor.h"
+
+#include <iostream>
+#include <ostream>
+
+#include "delpi/util/error.h"
+
+namespace delpi::internal {
+
+template <class T>
+RetriangularisationFactor<T>::RetriangularisationFactor(T value, const int row, const bool permuted,
+                                                        const bool inverted)
+    : value_{std::move(value)}, row_{row}, permuted_{permuted}, inverted_{inverted} {
+  DELPI_ASSERT(row >= 0, "Row index cannot be negative");
+}
+template <class T>
+Matrix<T>& operator*=(Matrix<T>& matrix, const RetriangularisationFactor<T>& factor) {
+  DELPI_ASSERT(factor.row() < matrix.rows() - 1, "Invalid row index");
+  const int pivot_row = factor.row();
+
+  // Swap the columns if the factor is permuted and inverted
+  if (factor.permuted() && factor.inverted()) matrix.col(pivot_row).swap(matrix.col(pivot_row + 1));
+
+  matrix.col(pivot_row) += matrix.col(pivot_row + 1) * factor.value();
+
+  // Swap the columns if the factor is permuted and not inverted
+  if (factor.permuted() && !factor.inverted()) matrix.col(pivot_row).swap(matrix.col(pivot_row + 1));
+
+  return matrix;
+}
+template <class T>
+Matrix<T>& operator*=(const RetriangularisationFactor<T>& factor, Matrix<T>& matrix) {
+  DELPI_ASSERT(factor.row() < matrix.rows() - 1, "Invalid row index");
+  const int pivot_row = factor.row();
+  // Swap the rows if the factor is permuted and not inverted
+  if (factor.permuted() && !factor.inverted()) matrix.row(pivot_row).swap(matrix.row(pivot_row + 1));
+
+  // Update the pivot row
+  matrix.row(pivot_row + 1) += matrix.row(pivot_row) * factor.value();
+
+  // Swap the rows if the factor is permuted and inverted
+  if (factor.permuted() && factor.inverted()) matrix.row(pivot_row).swap(matrix.row(pivot_row + 1));
+
+  return matrix;
+}
+
+template <class T>
+Matrix<T> operator*(const Matrix<T>& matrix, const RetriangularisationFactor<T>& factor) {
+  Matrix<T> copy_matrix = matrix;
+  copy_matrix *= factor;
+  return copy_matrix;
+}
+template <class T>
+Matrix<T> operator*(const RetriangularisationFactor<T>& factor, const Matrix<T>& matrix) {
+  Matrix<T> copy_matrix = matrix;
+  factor *= copy_matrix;
+  return copy_matrix;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const RetriangularisationFactor<T>& factor) {
+  Matrix<T> matrix = Matrix<T>::Identity(factor.row() + 2, factor.row() + 2);
+  if (factor.permuted()) {
+    matrix(factor.row() + 1, factor.row() + 1) = factor.value();
+    matrix(factor.row(), factor.row() + 1) = static_cast<T>(1);
+  } else {
+    matrix(factor.row() + 1, factor.row()) = factor.value();
+  }
+  return os << matrix;
+}
+
+template class RetriangularisationFactor<mpq_class>;
+template class RetriangularisationFactor<double>;
+
+template Matrix<mpq_class>& operator*=(Matrix<mpq_class>&, const RetriangularisationFactor<mpq_class>&);
+template Matrix<double>& operator*=(Matrix<double>&, const RetriangularisationFactor<double>&);
+
+template Matrix<mpq_class>& operator*=(const RetriangularisationFactor<mpq_class>&, Matrix<mpq_class>&);
+template Matrix<double>& operator*=(const RetriangularisationFactor<double>&, Matrix<double>&);
+
+template Matrix<mpq_class> operator*(const Matrix<mpq_class>&, const RetriangularisationFactor<mpq_class>&);
+template Matrix<double> operator*(const Matrix<double>&, const RetriangularisationFactor<double>&);
+template Matrix<mpq_class> operator*(const RetriangularisationFactor<mpq_class>&, const Matrix<mpq_class>&);
+template Matrix<double> operator*(const RetriangularisationFactor<double>&, const Matrix<double>&);
+
+template std::ostream& operator<<(std::ostream&, const RetriangularisationFactor<mpq_class>&);
+template std::ostream& operator<<(std::ostream&, const RetriangularisationFactor<double>&);
+
+}  // namespace delpi::internal
---
+++ tests/solver/TestRetriangularisationFactor.cpp
@@ -0,0 +1,199 @@
+/**
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024 delpi
+ * @licence BSD 3-Clause License
+ */
+#include <gtest/gtest.h>
+
+#include "delpi/solver/internal/RetriangularisationFactor.h"
+#include "delpi/util/exception.h"
+
+using delpi::DelpiAssertionException;
+using delpi::Matrix;
+using delpi::Vector;
+using delpi::internal::RetriangularisationFactor;
+
+template <class T>
+class TestRetriangularisationFactor : public ::testing::Test {
+ protected:
+  Matrix<T> matrix_{4, 4};
+  Matrix<T> L_;
+  Matrix<T> U_;
+  const T value_ = 2;
+  TestRetriangularisationFactor() {
+    matrix_ << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15;
+    L_ = matrix_.template triangularView<Eigen::Lower>().toDenseMatrix();
+    U_ = matrix_.template triangularView<Eigen::Upper>().toDenseMatrix();
+    U_.diagonal(-1) = Vector<T>::Ones(3);
+  }
+
+  Matrix<T> GetMatrixFactor(const int pivot_row, const bool permute = false) {
+    Eigen::MatrixX<T> matrix_factor = Eigen::MatrixX<T>::Identity(L_.rows(), L_.cols());
+    matrix_factor(pivot_row + 1, pivot_row) = this->value_;
+    Eigen::PermutationMatrix<Eigen::Dynamic> P(L_.rows());
+    P.setIdentity();
+    if (permute) P.applyTranspositionOnTheRight(pivot_row, pivot_row + 1);
+    return matrix_factor * P;
+  }
+};
+
+using types = ::testing::Types<double, mpq_class>;
+TYPED_TEST_SUITE(TestRetriangularisationFactor, types);
+
+TYPED_TEST(TestRetriangularisationFactor, LowerTriangularTimesFactor) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->L_ * this->GetMatrixFactor(pivot_row), this->L_ * factor);
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, LowerTriangularTimesFactorInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->L_ * this->GetMatrixFactor(pivot_row).inverse(), this->L_ * factor.Inverse());
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, LowerTriangularTimesFactorPermuted) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->L_ * this->GetMatrixFactor(pivot_row, true), this->L_ * factor.Permutation());
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, LowerTriangularTimesFactorPermutedInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->L_ * this->GetMatrixFactor(pivot_row, true).inverse(), this->L_ * factor.Permutation().Inverse());
+  }
+}
+
+#ifndef NDEBUG
+TYPED_TEST(TestRetriangularisationFactor, LowerTriangularTimesFactorInvalidRow) {
+  for (int i = 0; i < this->L_.cols() + 1; ++i) {
+    RetriangularisationFactor<TypeParam> factor{0, i};
+    if (i >= this->L_.cols() - 1) {
+      EXPECT_THROW(this->L_ * factor, DelpiAssertionException);
+    } else {
+      EXPECT_NO_THROW(this->L_ * factor);
+    }
+  }
+}
+#endif
+
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesUpperTriangular) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row) * this->U_, factor * this->U_);
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesUpperTriangularInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row).inverse() * this->U_, factor.Inverse() * this->U_);
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesUpperTriangularPermuted) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row, true) * this->U_, factor.Permutation() * this->U_);
+  }
+}
+
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesUpperTriangularPermutedInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row, true).inverse() * this->U_, factor.Permutation().Inverse() * this->U_);
+  }
+}
+
+#ifndef NDEBUG
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesUpperTriangularInvalidRow) {
+  for (int i = 0; i < this->U_.cols() + 1; ++i) {
+    RetriangularisationFactor<TypeParam> factor{0, i};
+    if (i >= this->U_.cols() - 1) {
+      EXPECT_THROW(factor * this->U_, DelpiAssertionException);
+    } else {
+      EXPECT_NO_THROW(factor * this->U_);
+    }
+  }
+}
+#endif
+
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesMatrix) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row) * this->matrix_, factor * this->matrix_);
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesMatrixInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row).inverse() * this->matrix_, factor.Inverse() * this->matrix_);
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesMatrixPermuted) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row, true) * this->matrix_, factor.Permutation() * this->matrix_);
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesMatrixPermutedInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->GetMatrixFactor(pivot_row, true).inverse() * this->matrix_,
+              factor.Permutation().Inverse() * this->matrix_);
+  }
+}
+#ifndef NDEBUG
+TYPED_TEST(TestRetriangularisationFactor, FactorTimesMatrixInvalidRow) {
+  for (int i = 0; i < this->matrix_.cols() + 1; ++i) {
+    RetriangularisationFactor<TypeParam> factor{0, i};
+    if (i >= this->matrix_.cols() - 1) {
+      EXPECT_THROW(factor * this->matrix_, DelpiAssertionException);
+    } else {
+      EXPECT_NO_THROW(factor * this->matrix_);
+    }
+  }
+}
+#endif
+
+TYPED_TEST(TestRetriangularisationFactor, MatrixTimesFactor) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->matrix_ * this->GetMatrixFactor(pivot_row), this->matrix_ * factor);
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, MatrixTimesFactorInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->matrix_ * this->GetMatrixFactor(pivot_row).inverse(), this->matrix_ * factor.Inverse());
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, MatrixTimesFactorPermuted) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->matrix_ * this->GetMatrixFactor(pivot_row, true), this->matrix_ * factor.Permutation());
+  }
+}
+TYPED_TEST(TestRetriangularisationFactor, MatrixTimesFactorPermutedInverse) {
+  for (int pivot_row = 0; pivot_row < this->L_.cols() - 1; ++pivot_row) {
+    RetriangularisationFactor<TypeParam> factor{this->value_, pivot_row};
+    EXPECT_EQ(this->matrix_ * this->GetMatrixFactor(pivot_row, true).inverse(),
+              this->matrix_ * factor.Permutation().Inverse());
+  }
+}
+#ifndef NDEBUG
+TYPED_TEST(TestRetriangularisationFactor, MatrixTimesFactorInvalidRow) {
+  for (int i = 0; i < this->matrix_.cols() + 1; ++i) {
+    RetriangularisationFactor<TypeParam> factor{0, i};
+    if (i >= this->matrix_.cols() - 1) {
+      EXPECT_THROW(this->matrix_ * factor, DelpiAssertionException);
+    } else {
+      EXPECT_NO_THROW(this->matrix_ * factor);
+    }
+  }
+}
+#endif
