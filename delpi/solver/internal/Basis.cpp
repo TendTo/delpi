51e0450b0e035b591bea119ae0862a1e72ddeb33
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-01-29 18:26:46 +0000

    chore: add assertions and offset indexes method for basis

--- delpi/solver/internal/Basis.cpp
+++ delpi/solver/internal/Basis.cpp
@@ -38,7 +38,13 @@ Basis<T>::Basis(const Eigen::MatrixX<T>& A, const std::shared_ptr<std::vector<in
 
 template <class T>
 void Basis<T>::Update(const Eigen::MatrixX<T>& A, const int leaving, const int entering) {
+  DELPI_ASSERT(leaving >= 0 && leaving < A.rows(), "Invalid leaving index");
+  DELPI_ASSERT(entering >= 0 && entering < A.cols(), "Invalid entering index");
   DELPI_TRACE_FMT("Basis::Update(leaving = {}, entering = {})", leaving, entering);
+
+  // Ensure that no other instance sharing the same basis indexes is affected
+  if (basis_idxs_.use_count() > 1) basis_idxs_ = std::make_shared<std::vector<int>>(*basis_idxs_);
+
   last_basis_leaving_ = leaving;
   // TODO(tend): the place where the basis is updated could be customised by a subclass
   last_basis_entering_ = basis_vectors_.cols() - 1;
@@ -46,11 +52,16 @@ void Basis<T>::Update(const Eigen::MatrixX<T>& A, const int leaving, const int e
   last_entering_ = entering;
 
   // Move all columns to the right of the leaving column one position to the left
-  if (leaving < basis_vectors_.cols() - 1) {
-    std::rotate(basis_idxs_->begin() + leaving, basis_idxs_->begin() + leaving + 1, basis_idxs_->end());
-    basis_idxs_->back() = entering;
-    basis_vectors_ = A(Eigen::all, *basis_idxs_);
-  }
+  std::rotate(basis_idxs_->begin() + leaving, basis_idxs_->begin() + leaving + 1, basis_idxs_->end());
+  // Set the last basis column to the entering column
+  basis_idxs_->back() = entering;
+  basis_vectors_ = A(Eigen::all, *basis_idxs_);
+}
+template <class T>
+void Basis<T>::OffsetIndexes(const int offset) {
+  // Ensure that no other instance sharing the same basis indexes is affected
+  if (basis_idxs_.use_count() > 1) basis_idxs_ = std::make_shared<std::vector<int>>(*basis_idxs_);
+  for (auto& idx : *basis_idxs_) idx += offset;
 }
 
 template <class T>
