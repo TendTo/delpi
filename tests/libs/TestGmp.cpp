be5ffe488dc4ac9e6e3e8ac8f31079c9b7a92fe2
Ernesto Casablanca <casablancaernesto@gmail.com>
2026-05-30 20:09:53 +0100

    test: add gmp test suite

---
+++ tests/libs/TestGmp.cpp
@@ -0,0 +1,124 @@
+/**
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024 delpi
+ * @licence BSD 3-Clause License
+ */
+#include <gtest/gtest.h>
+
+#include "delpi/libs/gmp.h"
+
+using delpi::gmp::ceil;
+using delpi::gmp::floor;
+using delpi::gmp::StringToMpq;
+using delpi::gmp::ToMpq;
+using delpi::gmp::ToMpqClass;
+using std::make_pair;
+
+class TestGmp : public ::testing::TestWithParam<std::pair<std::string, mpq_class>> {};
+
+INSTANTIATE_TEST_SUITE_P(TestGmp, TestGmp,
+                         ::testing::Values(make_pair("0", mpq_class{0}), make_pair(".", mpq_class{0}),
+                                           make_pair("0.", mpq_class{0}), make_pair("0.0", mpq_class{0}),
+                                           make_pair(".0", mpq_class{0}), make_pair("15", mpq_class{15}),
+                                           make_pair("1.5", mpq_class{15, 10}), make_pair("0000015.", mpq_class{15}),
+                                           make_pair(".15", mpq_class{15, 100}), make_pair(".15", mpq_class{15, 100}),
+                                           make_pair(".0015", mpq_class{15, 10000}), make_pair("15.0", mpq_class{15}),
+                                           make_pair("15.00", mpq_class{15}), make_pair("0150", mpq_class{150}),
+                                           make_pair("1.5E2", mpq_class{150}), make_pair("1.5E-2", mpq_class{15, 1000}),
+                                           make_pair(".e+2", mpq_class{0}), make_pair(".5E+2", mpq_class{50}),
+                                           make_pair("000000.5E+2", mpq_class{50}),
+                                           make_pair("000000.005E-2", mpq_class{5, 100000}),
+                                           make_pair("E+2", mpq_class{100}), make_pair("7E+2", mpq_class{700}),
+                                           make_pair("E-2", mpq_class{1, 100}), make_pair("15/6", mpq_class{15, 6}),
+                                           make_pair("0/1010", mpq_class{0}), make_pair("", mpq_class{0}),
+                                           make_pair("inf", mpq_class{1e100})));
+
+TEST(TestGmp, FloorFractions) {
+  EXPECT_EQ(floor(mpq_class{1, 2}), 0);
+  EXPECT_EQ(floor(mpq_class{3, 2}), 1);
+  EXPECT_EQ(floor(mpq_class{7, 3}), 2);
+  EXPECT_EQ(floor(mpq_class{-1, 2}), -1);
+  EXPECT_EQ(floor(mpq_class{-3, 2}), -2);
+  EXPECT_EQ(floor(mpq_class{-7, 3}), -3);
+}
+
+TEST(TestGmp, FloorIntegers) {
+  EXPECT_EQ(floor(mpq_class{1}), 1);
+  EXPECT_EQ(floor(mpq_class{-1}), -1);
+  EXPECT_EQ(floor(mpq_class{0}), 0);
+}
+
+TEST(TestGmp, CeilFractions) {
+  EXPECT_EQ(ceil(mpq_class{1, 2}), 1);
+  EXPECT_EQ(ceil(mpq_class{3, 2}), 2);
+  EXPECT_EQ(ceil(mpq_class{7, 3}), 3);
+  EXPECT_EQ(ceil(mpq_class{-1, 2}), 0);
+  EXPECT_EQ(ceil(mpq_class{-3, 2}), -1);
+  EXPECT_EQ(ceil(mpq_class{-7, 3}), -2);
+}
+
+TEST(TestGmp, CeilIntegers) {
+  EXPECT_EQ(ceil(mpq_class{1}), 1);
+  EXPECT_EQ(ceil(mpq_class{-1}), -1);
+  EXPECT_EQ(ceil(mpq_class{0}), 0);
+}
+
+TEST(TestGmp, ToMpqClass) {
+  mpq_t a;
+  mpq_init(a);
+
+  mpq_set_d(a, 1);
+  EXPECT_EQ(ToMpqClass(a), mpq_class{1});
+
+  mpq_set_d(a, 0);
+  EXPECT_EQ(ToMpqClass(a), mpq_class{0});
+
+  mpq_set_si(a, 1, 2);
+  EXPECT_EQ(ToMpqClass(a), mpq_class(1, 2));
+
+  mpq_clear(a);
+}
+
+TEST(TestGmp, ToMpqT) {
+  mpq_class a{1};
+  mpq_t b;
+  mpq_init(b);
+
+  mpq_set_d(b, 1);
+  EXPECT_TRUE(mpq_equal(ToMpq(a), b));
+
+  a = 0;
+  mpq_set_d(b, 0);
+  EXPECT_TRUE(mpq_equal(ToMpq(a), b));
+
+  a = mpq_class{1, 2};
+  mpq_set_si(b, 1, 2);
+  EXPECT_TRUE(mpq_equal(ToMpq(a), b));
+
+  mpq_clear(b);
+}
+
+TEST_P(TestGmp, ConvertStringToMpq) {
+  auto [s, expected] = GetParam();
+  expected.canonicalize();
+  EXPECT_EQ(StringToMpq(s), expected);
+}
+
+TEST_P(TestGmp, ConvertStringToMpqPrefixPlus) {
+  auto [s, expected] = GetParam();
+  expected.canonicalize();
+  EXPECT_EQ(StringToMpq("+" + s), expected);
+}
+
+TEST_P(TestGmp, ConvertStringToMpqPrefixMinus) {
+  auto [s, expected] = GetParam();
+  expected.canonicalize();
+  EXPECT_EQ(StringToMpq("-" + s), -expected);
+}
+
+TEST_P(TestGmp, ConvertStringToMpqSuffixOther) {
+  auto [s, expected] = GetParam();
+  std::string new_string = s + "abc and other stuff";
+  expected.canonicalize();
+  EXPECT_EQ(StringToMpq(std::string_view{new_string.data(), s.size()}), expected);
+}
